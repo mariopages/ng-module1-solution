f74b79f6def97b84ae92f3d6bc5d54273f7a5c9a
Mario Pages <Mario Pages>
2017-05-12 14:43:00 -0400

    Final files

--- app.js
+++ app.js
@@ -3,8 +3,40 @@
 
 angular.module('LunchCheck', [])
 
-.controller('LunchCheckController', function () {
+.controller('LunchCheckController', LunchCheckController);
 
-});
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+  $scope.formholder="";
+  $scope.msgstate='';
+
+  function splitString(stringToSplit, separator) {
+    var arrayOfStrings = stringToSplit.split(separator);
+
+    if(arrayOfStrings.length > 3) {
+      $scope.sayMessage = "Too much!";
+      $scope.msgstate='successmsg';
+    }
+    else {
+      if (arrayOfStrings=="") {
+        $scope.sayMessage = 'Please enter data first';
+        $scope.msgstate='warningmsg';
+      }
+      else {
+        $scope.sayMessage = 'Enjoy!';
+        $scope.msgstate='successmsg';
+      }
+    }
+  }
+var comma = ',';
+
+  $scope.toomuch = function () {
+    if (typeof $scope.formholder != 'string') {
+    $scope.formholder = "";
+    }
+    splitString($scope.formholder, comma);
+  }
+};
 
 })();
